02ea95e9ef32917f00ca531b4316998fd0d1b931
Hammad. A <140536967+Savant-madi@users.noreply.github.com>
2024-03-14 00:27:17 +0500

    Basics of an array

---
+++ array.js
@@ -0,0 +1,44 @@
+// arrays
+const myArr = [0, 1, 2, 3, 4, 5];
+const myH = ["hi", "gg", "okay"];
+const myArr2 = new Array(1, 2, 3, 4, 44); //creating an array
+// console.log(myArr2);
+
+//array methods
+
+myArr.push(6); //adds the value at the last point
+myArr.pop(); // removes last value
+// console.log(myArr);
+// myArr.unshift(9); // adds the number in the start
+// console.log(myArr);
+// myArr.shift() // removes the first value
+// console.log(myArr);
+
+// console.log(myArr.includes(2)); // checks the value
+// console.log(myArr.indexOf(2));// this also the value if not in array it will give -1 otherwise tells the index of that value
+
+
+// const newArr=myArr.join() // it will convert the array into string
+// console.log(myArr);
+// console.log(newArr);
+
+
+
+
+//slice and splice
+
+// console.log("A",myArr);
+
+// const myn1=myArr.slice(1,3) // slice will give you that specfic range of values you set
+// console.log(myn1);
+// console.log("B",myArr);
+
+// const myn2=myArr.splice(1,3) // splice removes other value than in the range like in this it will the values at index 1,2,3 and will give 0,4,5
+// console.log("C",myArr);
+// console.log(myn2);
+
+
+
+
+
+
