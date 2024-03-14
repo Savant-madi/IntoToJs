6db752d9a6c33001456e80489f991a8606dc92d0
Hammad. A <140536967+Savant-madi@users.noreply.github.com>
2024-03-14 22:18:21 +0500

    Intro to functions
    Basic understanding of functions

---
+++ functions.js
@@ -0,0 +1,82 @@
+//Functions
+
+// function addTwoNum(n1,n2){ //parameters
+//     console.log(n1+n2);
+    
+// }
+// function addTwoNum(n1,n2){ 
+    
+//     // let result=n1+n2
+    
+//     return n1+n2
+    
+// }
+
+// const result=addTwoNum(4,5)//arguments
+
+// console.log(result);
+
+
+
+// function loginUserMessage(username="ham"){ //ham is used as default value if no value is needed
+//     if(!username){
+//         console.log("pls enter usernae");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+
+
+// console.log(loginUserMessage("hammad"))
+// console.log(loginUserMessage()) // if you dont pass any arguments to the function the result will be undefined
+
+
+
+
+
+
+
+
+
+
+
+function calculateCartPrice(val1,val2,...num1){ // the three dots are rest operator which are same as spread operator just using them in different scenarios change the function of these this is mostly used in shopping carts when you dont know how much items will the customer will put in the cart  and will put them in array
+    return num1
+}
+
+// console.log(calculateCartPrice(2,200,400));
+
+//PUTTING OBJECT IN FUNCTION............
+
+// const user={
+//     username:"hammad",
+//     price:200
+// }
+
+// function handleobject(anyobject){
+//     console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+// }
+
+// handleobject(user)
+
+
+// PASSING ARRAY IN FUCTION......
+
+
+
+// const myNewArray=[200,400,100,600]
+
+// function returnSecondValue(getArray){
+//     return getArray[3]
+// }
+
+// console.log(returnSecondValue(myNewArray));
+
+
+
+
+
+
+
+
+
